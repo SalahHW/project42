aebddbf818ce6a39a4ab7e7c5efc43c418e9e28b
Salah <salah.bouheniche@gmail.com>
2022-11-07 17:57:38 +0100

    add ft_isascii()

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: sbouheni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 11:08:51 by sbouheni          #+#    #+#             */
-/*   Updated: 2022/11/07 11:16:15 by sbouheni         ###   ########.fr       */
+/*   Updated: 2022/11/07 17:54:20 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,7 @@
 
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
 
 #endif
