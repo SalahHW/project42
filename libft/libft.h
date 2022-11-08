546090b5956343f932416fc155fe14ef2a87e8c5
Salah <salah.bouheniche@gmail.com>
2022-11-08 17:09:45 +0100

    added ft_isprint.c

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: sbouheni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 11:08:51 by sbouheni          #+#    #+#             */
-/*   Updated: 2022/11/07 17:54:20 by sbouheni         ###   ########.fr       */
+/*   Updated: 2022/11/08 16:57:04 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,6 @@ int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 int	ft_isalnum(int c);
 int	ft_isascii(int c);
+int ft_isprint(int c);
 
 #endif
