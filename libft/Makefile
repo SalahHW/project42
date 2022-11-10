be52fef5b58482585db31927d0010cfb035acf98
Salah <salah.bouheniche@gmail.com>
2022-11-10 22:42:29 +0100

    add ft_bzero.c

--- libft/Makefile
+++ libft/Makefile
@@ -6,12 +6,12 @@
 #    By: sbouheni <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/11/07 11:04:11 by sbouheni          #+#    #+#              #
-#    Updated: 2022/11/09 08:37:18 by sbouheni         ###   ########.fr        #
+#    Updated: 2022/11/10 22:15:47 by sbouheni         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 SOURCES = ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_isascii.c ft_isprint.c \
-					ft_strlen.c ft_memset.c
+					ft_strlen.c ft_memset.c ft_bzero.c
 
 NAME = libft.a
 
--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -1,25 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sbouheni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/07 11:08:51 by sbouheni          #+#    #+#             */
-/*   Updated: 2022/11/10 22:05:36 by sbouheni         ###   ########.fr       */
+/*   Created: 2022/11/10 22:09:32 by sbouheni          #+#    #+#             */
+/*   Updated: 2022/11/10 22:20:56 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <string.h>
+void	ft_bzero(void *s, size_t n)
+{
+	size_t	i;
 
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-int		ft_strlen(const char *s);
-void	*ft_memset(void *s, int c, size_t n);
-#endif
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char *)s)[i] = 0;
+		i++;
+	}
+}
