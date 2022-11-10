a8c5d57f73308f9b776fbab4487b0f30fe2733e0
Salah <salah.bouheniche@gmail.com>
2022-11-10 11:46:51 +0100

    added ft_memset.c

--- libft/libft.h
+++ libft/libft.h
@@ -6,12 +6,14 @@
 /*   By: sbouheni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 11:08:51 by sbouheni          #+#    #+#             */
-/*   Updated: 2022/11/08 17:35:41 by sbouheni         ###   ########.fr       */
+/*   Updated: 2022/11/10 11:46:37 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
+
+#include <stddef.h>
 
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
@@ -19,5 +21,5 @@ int	ft_isalnum(int c);
 int	ft_isascii(int c);
 int	ft_isprint(int c);
 int	ft_strlen(const char *s);
-
+void	*ft_memset(void *s, int c, size_t n);
 #endif
