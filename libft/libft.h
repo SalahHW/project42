ff63c85c2f421f4d50623646d8f0b1dfa6d7c659
Salah <salah.bouheniche@gmail.com>
2022-11-11 11:38:20 +0100

    fix norminette errors

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: sbouheni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 11:08:51 by sbouheni          #+#    #+#             */
-/*   Updated: 2022/11/10 22:05:36 by sbouheni         ###   ########.fr       */
+/*   Updated: 2022/11/11 11:37:48 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,6 @@ int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
 int		ft_strlen(const char *s);
-void		*ft_memset(void *s, int c, int n);
-void		ft_bzero(void *s, int n);
+void	*ft_memset(void *s, int c, int n);
+void	ft_bzero(void *s, int n);
 #endif
