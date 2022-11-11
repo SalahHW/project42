cb9b9987fda9819910fb6a39bd1c15a4f8eccf16
SalahHW <salah.bouheniche@gmail.com>
2022-11-11 11:34:22 +0100

    changed size_t into int

--- libft/libft.h
+++ libft/libft.h
@@ -13,13 +13,12 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# include <string.h>
-
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
 int		ft_strlen(const char *s);
-void	*ft_memset(void *s, int c, size_t n);
+void		*ft_memset(void *s, int c, int n);
+void		ft_bzero(void *s, int n);
 #endif
