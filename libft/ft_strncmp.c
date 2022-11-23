c0d9744f4da141b6e4a4df75e634a769d3cb7832
SalahHW <salah.bouheniche@gmail.com>
2022-11-23 13:19:14 +0100

    completed ft_strncmp

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: sbouheni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 22:12:34 by sbouheni          #+#    #+#             */
-/*   Updated: 2022/11/21 23:26:48 by sbouheni         ###   ########.fr       */
+/*   Updated: 2022/11/23 13:11:34 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,16 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while ((i < n - 1) && (s1[i] == s2[i]) && (s1[i]) && (s2[i]))
-	{	
-		if (n == 0 || s1[i] == '\0' || s2[i] =='\0')
-			break;
+	while (i < n && (s1[i] || s2[i]))
+	{
+		if (s1[i] != s2[i])
+		{	
+			if ((((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]) > 0)
+				return (1);
+			if ((((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]) < 0)
+				return (-1);
+		}
 		i++;
 	}
-	if ((s1[i] - s2[i]) > 0)
-		return (1);
-	if ((s1[i] - s2[i]) < 0)
-		return (-1);
 	return (0);
 }
