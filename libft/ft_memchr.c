f18ef377e8c2a26ca2a5fc128b29fd633a8cdfec
SalahHW <salah.bouheniche@gmail.com>
2022-11-25 16:36:02 +0100

    fixed norminette errors

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: sbouheni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 00:52:36 by sbouheni          #+#    #+#             */
-/*   Updated: 2022/11/25 04:08:59 by sbouheni         ###   ########.fr       */
+/*   Updated: 2022/11/25 16:35:29 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t					i;
-	unsigned char		unchar_c;
-	unsigned char		*unchar_s;
+	size_t			i;
+	unsigned char	unchar_c;
+	unsigned char	*unchar_s;
 
 	i = 0;
 	unchar_c = (unsigned char)c;
