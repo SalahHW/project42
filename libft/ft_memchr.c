285641a8900233d5a361878b9c40b17cbb09a0d4
Salah <salah.bouheniche@gmail.com>
2022-11-25 04:11:41 +0100

    updated ft_memchr.c

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -1,17 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbouheni <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/25 00:52:36 by sbouheni          #+#    #+#             */
+/*   Updated: 2022/11/25 04:08:59 by sbouheni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	i;
-	
+	size_t					i;
+	unsigned char		unchar_c;
+	unsigned char		*unchar_s;
+
 	i = 0;
+	unchar_c = (unsigned char)c;
+	unchar_s = (unsigned char *)s;
 	while (i < n)
 	{
-		if (((unsigned char *)s)[i] == (unsigned char)c)
-		{
-			return ((void)*s[i]);
-		} 
+		if (unchar_s[i] == unchar_c)
+			return ((void *)s + i);
 		i++;
 	}
 	return (NULL);
