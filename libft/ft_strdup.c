84294e18c865317b8688485b5aebbf0757171360
SalahHW <salah.bouheniche@gmail.com>
2022-11-29 19:29:31 +0100

    fixed norminette errors

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: sbouheni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 18:23:41 by sbouheni          #+#    #+#             */
-/*   Updated: 2022/11/29 19:17:02 by sbouheni         ###   ########.fr       */
+/*   Updated: 2022/11/29 19:28:59 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 char	*ft_strdup(const char *s1)
 {
-	char *new_str;
+	char	*new_str;
 
 	new_str = malloc(ft_strlen(s1) + 1);
-	if	(new_str)
+	if (new_str)
 	{
 		ft_strlcpy(new_str, s1, ft_strlen(s1) + 1);
 		return (new_str);
