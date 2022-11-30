146e8c1832e011daa466ed85fc1a2a1d503577d6
Salah <salah.bouheniche@gmail.com>
2022-11-30 22:17:10 +0100

    unfinished function

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -16,19 +16,11 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*new_str;
 
-	if (start < ft_strlen(s))
+	s += start;
+	new_str = malloc(len + 1);
+	if (new_str)
 	{
-		s += start;
-		if (len >= ft_strlen(s))
-		{
-			new_str = malloc(len);
-			ft_strlcpy(new_str, s, len + 1);
-		}
-		else 
-		{
-			new_str = malloc(ft_strlen(s) + 1);
-			ft_strlcpy(new_str, s, ft_strlen(s) + 1);
-		}
+		ft_strlcpy(new_str, s, len + 1);
 		return (new_str);
 	}
 	return (NULL);
