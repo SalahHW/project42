5d0ea003136f3ca4178ac16251e5deae659772dd
Salah <salah.bouheniche@gmail.com>
2022-12-02 06:48:28 +0100

    fixed norminette errors

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -14,16 +14,16 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
+	char	*newstring;
+
 	if (s1 && s2)
 	{
-		char *newstring;
-
 		newstring = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
 		if (newstring)
 		{
 			*newstring = '\0';
 			ft_strlcat(newstring, s1, ft_strlen(s1) + 1);
-			ft_strlcat(newstring, s2, ft_strlen(s1) + ft_strlen(s2)  + 1);
+			ft_strlcat(newstring, s2, ft_strlen(s1) + ft_strlen(s2) + 1);
 			return (newstring);
 		}
 	}
