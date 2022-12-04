1219e0fb1eff86efd09ba7d39c0107b08da562b8
Salah <salah.bouheniche@gmail.com>
2022-12-04 03:24:54 +0100

    uncomplete fuction

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -29,37 +29,52 @@ int	word_counter(const char *str, char separation)
 	return (words);
 }
 
+char	*allocate_substring(char const *str, size_t start, size_t i)
+{
+	char	*allocated_substring;
+
+	allocated_substring = malloc(sizeof(ft_substr(str, start, i - start)));
+	if (allocated_substring)
+	{
+		allocated_substring = ft_substr(str, start, i - start);
+		return (allocated_substring);
+	}
+	return (NULL);
+}
+
 char	**ft_split(char const *s, char c)
 {
+	size_t	start;
 	size_t	i;
-	size_t	j;
 	size_t	k;
-	char	*temp_string;
 	char	**substring;
 
 	if (s && c)
 	{
 		i = 0;
-		j = 0;
+		start = 0;
 		k = 0;
 		substring = malloc(word_counter(s, c)+ 1);
 		if (substring)
 		{
 			while (s[i])
 			{
-				if (s[i] == c)
+				if (s[i] != c)
 				{
-					temp_string = malloc(sizeof(ft_substr(s, j, i)));	
-					if (temp_string)
-					{
-						temp_string = ft_substr(s, j, i);
-						j = i + 1;
-						substring[k] = temp_string;
-						k++;
-					}
+					i++;
+					continue;
 				}
+				*substring = allocate_substring(s, start, i);
+				start = i + 1;
+				*substring += 1;
+				k++;
 				i++;
 			}
+			*substring += 1;
+			*substring = allocate_substring(s, start, i);
+			*substring += 1;
+			*substring = NULL;
+			return (substring - k - 2);
 		}
 	}
 	return (NULL);
