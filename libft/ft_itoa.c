cbd99eaa0f280848302aa08040f142918411c99a
SalahHW <salah.bouheniche@gmail.com>
2022-12-08 04:22:50 +0100

    almost working function

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -6,31 +6,46 @@
 /*   By: sbouheni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 04:51:02 by sbouheni          #+#    #+#             */
-/*   Updated: 2022/12/07 22:30:39 by sbouheni         ###   ########.fr       */
+/*   Updated: 2022/12/08 04:21:05 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	is_negative(int n)
+{
+	if (n < 0)
+		return (1);
+	return (0);
+}
+
 static int	number_length(int number)
 {
-	int	length;
-	int	temp;
+	int		length;
+	long	temp;
+	long	n;
 
+	n = (long)number;
 	length = 0;
 	temp = 1;
-	while (temp <= number)
+	if (is_negative(number) || number == 0)
+	{
+		length++;
+		n = -n;
+	}
+	while (temp <= n)
 	{
 		length++;
 		temp *= 10;
 	}
+
 	return (length);
 }
 
 static int	get_last_digit(int number)
 {
 	int	last_digit;
-	
+
 	last_digit = number % 10;
 	return (last_digit);
 }
@@ -39,23 +54,27 @@ char	*ft_itoa(int n)
 {
 	char	*number;
 	int		i;
+	int		negative;
 
 	i = number_length(n);
-	number = malloc((i + 1) * sizeof(char));
+	number = malloc((number_length(n) + 1) * sizeof(char));
 	if (number)
 	{
 		number[i] = '\0';
-		i--;
-		while (i > 0)
+		if (is_negative(n))
 		{
-			while (n >= 10)
-			{
-				number[i] = get_last_digit(n) + '0';
-				n = n / 10;
-				i--;
-			}
-			number[i] = n + '0';
+			n =-n;
+			negative = 1;
 		}
+		i--;
+		while (i >= 0)
+		{
+			number[i] = get_last_digit(n) + '0';
+			n /= 10;
+			i--;
+		}
+		if (negative)
+			number[0] = '-';
 		return (number);
 	}
 	return (NULL);
