be921e186867dd9d44b67f229587a40f5ce64791
Salah <salah.bouheniche@gmail.com>
2022-12-19 07:30:30 +0100

    compelete function

--- ft_printf/src/ft_print_char.c
+++ ft_printf/src/ft_print_char.c
@@ -6,13 +6,15 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/18 02:41:57 by sbouheni          #+#    #+#             */
-/*   Updated: 2022/12/18 02:46:14 by sbouheni         ###   ########.fr       */
+/*   Updated: 2022/12/19 07:28:44 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/ft_printf.h"
+#include <unistd.h>
 
 int	ft_print_char(int ap)
 {
+	write(1, &ap, 1);
 	return (1);
 }
