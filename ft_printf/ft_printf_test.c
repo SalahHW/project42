e5717367c9b518819f1cc8e7e0da252bff337c76
Salah <salah.bouheniche@gmail.com>
2022-12-20 17:55:02 +0100

    testing file

--- ft_printf/ft_printf_test.c
+++ ft_printf/ft_printf_test.c
@@ -1,8 +1,12 @@
 #include "src/ft_printf.c"
+#include <stdio.h>
 
 int	main(void)
 {
+	int	length;
 	char *str = "Hello world!";
 	
-	ft_printf("%c", 'S');
+	length = ft_printf(" %i \n", 189334);
+	ft_printf("%i\n", length);
+	printf("%i", length);
 }
