6f50a7dadd2ac1b0a10ae63d24331764e93b20ea
Salah <salah.bouheniche@gmail.com>
2022-12-20 17:57:51 +0100

    work in progress

--- ft_printf/src/ft_print_integer.c
+++ ft_printf/src/ft_print_integer.c
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/18 02:49:57 by sbouheni          #+#    #+#             */
-/*   Updated: 2022/12/20 17:35:58 by sbouheni         ###   ########.fr       */
+/*   Updated: 2022/12/20 17:56:40 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,5 @@ int	ft_print_integer(int ap)
 
 	count = ft_strlen(ft_itoa(ap));
 	ft_putstr_fd(ft_itoa(ap), 1);
-
 	return (count);
 }
