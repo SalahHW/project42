374b960a3bb386169a332caea0739dc296167703
SalahHW <salah.bouheniche@gmail.com>
2022-12-27 15:24:04 +0100

    corrected size returned when invalid input are sent

--- ft_printf/src/ft_print_char.c
+++ ft_printf/src/ft_print_char.c
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/18 02:41:57 by sbouheni          #+#    #+#             */
-/*   Updated: 2022/12/26 15:16:23 by sbouheni         ###   ########.fr       */
+/*   Updated: 2022/12/27 15:21:04 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,6 @@
 
 int	ft_print_char(int ap)
 {
-	if (ap)
-	{
-		ft_putchar_fd(ap, 1);
-		return (1);
-	}
-	return (0);
+	ft_putchar_fd(ap, 1);
+	return (1);
 }
