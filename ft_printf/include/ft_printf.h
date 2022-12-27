202602108c3bf55b20222dce1736defd6449f090
SalahHW <salah.bouheniche@gmail.com>
2022-12-27 15:53:30 +0100

    update

--- ft_printf/include/ft_printf.h
+++ ft_printf/include/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 08:39:39 by sbouheni          #+#    #+#             */
-/*   Updated: 2022/12/27 12:42:27 by sbouheni         ###   ########.fr       */
+/*   Updated: 2022/12/27 15:50:43 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@
 
 int	ft_print_char(int ap);
 int	ft_print_string(char *ap);
-int	ft_print_hexadecimal(unsigned long long int ap);
+int	ft_print_hexadecimal(unsigned long long ap);
 int	ft_print_integer(int ap);
 int	ft_print_unsigned_decimal(unsigned int ap);
-int	ft_print_lower_hexadecimal(unsigned long long int ap);
-int	ft_print_upper_hexadecimal(unsigned long long int ap);
+int	ft_print_lower_hexadecimal(unsigned long long ap);
+int	ft_print_upper_hexadecimal(unsigned long long ap);
 int	ft_print_percent(void);
 int	ft_printf(const char *format, ...);
 
