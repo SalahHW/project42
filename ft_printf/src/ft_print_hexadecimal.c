365761546007bf7a5fe1e5ca84759a67b7ac2c56
Salah <salah.bouheniche@gmail.com>
2022-12-31 13:09:54 +0100

    add allocation for the printed string

--- ft_printf/src/ft_print_hexadecimal.c
+++ ft_printf/src/ft_print_hexadecimal.c
@@ -6,20 +6,35 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/18 02:46:21 by sbouheni          #+#    #+#             */
-/*   Updated: 2022/12/27 15:50:05 by sbouheni         ###   ########.fr       */
+/*   Updated: 2022/12/31 13:00:25 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "libft.h"
 
+static int	get_size(unsigned long long ap)
+{
+	int	count;
+
+	count = 0;
+	while (ap > 0)
+	{
+		ap /= 16;
+		count++;
+	}
+	return (count);
+}
+
 int	ft_print_hexadecimal(unsigned long long ap)
 {
-	char	hexadecimal_str[17];
+	char	*hexadecimal_str;
 	int		i;
 	int		digit;
+	int		size;
 
 	i = 0;
+	hexadecimal_str = malloc(sizeof(char) * (get_size(ap) + 1));
 	while (ap > 0)
 	{
 		digit = ap % 16;
@@ -37,7 +52,12 @@ int	ft_print_hexadecimal(unsigned long long ap)
 	}
 	hexadecimal_str[i] = '\0';
 	ft_putstr_fd("0x", 1);
-	while (i-- >= 0)
+	while (i >= 0)
+	{
 		ft_putchar_fd(hexadecimal_str[i], 1);
-	return (ft_strlen(hexadecimal_str) + 2);
+		i--;
+	}
+	size = ft_strlen(hexadecimal_str) + 2;
+	free(hexadecimal_str);
+	return (size);
 }
