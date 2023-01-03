f3a189166f0dffc0be57d01c4c8cc2df4efd5466
Salah <salah.bouheniche@gmail.com>
2023-01-03 02:58:31 +0100

    update test

--- ft_printf/ft_printf_test.c
+++ ft_printf/ft_printf_test.c
@@ -6,8 +6,8 @@ int	main(void)
 	int	length;
 	char *str = "Hello World!";
 	
-	length = ft_printf("%u, %s, %p\n", -150, str, str);
+	length = ft_printf("%u, %s, %p, %c\n", -150, str, str, 'S');
 	ft_printf("%i\n", length);
-	length = printf("%u, %s, %p\n", -150, str, str);
+	length = printf("%u, %s, %p, %c\n", -150, str, str, 'S');
 	printf("%i\n", length);
 }
