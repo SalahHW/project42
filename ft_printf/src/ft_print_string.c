2dad04b4f23cc038ce6fb09b321046e7703c1c1f
SalahHW <salah.bouheniche@gmail.com>
2023-01-04 16:47:34 +0100

    add (null) return when empty string is sent

--- ft_printf/src/ft_print_string.c
+++ ft_printf/src/ft_print_string.c
@@ -6,12 +6,13 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/18 02:58:35 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/01/04 16:23:27 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/01/04 16:42:51 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "libft.h"
+
 
 int	ft_print_string(char *ap)
 {
@@ -19,12 +20,13 @@ int	ft_print_string(char *ap)
 	char *null_str;
 
 	count = 0;
-	null_str = "(null).";
+	null_str = "(null)";
 	if (ap)
 	{
 		while (ap[count])
 		{
-			ft_print_char(ap[count]);
+			if (ft_print_char(ap[count]) < 0)
+				return (-2147483648);
 			count++;
 		}
 	}
@@ -32,7 +34,8 @@ int	ft_print_string(char *ap)
 	{
 		while (null_str[count])
 		{
-			ft_print_char(null_str[count]);
+			if (ft_print_char(null_str[count]) < 0)
+				return (-2147483648);
 			count++;
 		}
 	}
