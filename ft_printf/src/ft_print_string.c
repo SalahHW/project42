0242c273099ad49d4b3c822e5b69c5bcda9bfd4f
Salah <salah.bouheniche@gmail.com>
2023-01-04 16:25:01 +0100

    fix value returned when string is null

--- ft_printf/src/ft_print_string.c
+++ ft_printf/src/ft_print_string.c
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/18 02:58:35 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/01/04 00:20:34 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/01/04 16:23:27 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,23 @@
 int	ft_print_string(char *ap)
 {
 	int	count;
+	char *null_str;
 
 	count = 0;
+	null_str = "(null).";
 	if (ap)
 	{
 		while (ap[count])
 		{
 			ft_print_char(ap[count]);
+			count++;
+		}
+	}
+	else
+	{
+		while (null_str[count])
+		{
+			ft_print_char(null_str[count]);
 			count++;
 		}
 	}
