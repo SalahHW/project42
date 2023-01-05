1d538d4ec21cb3fbf30aca99e8a1918193d43d92
SalahHW <salah.bouheniche@gmail.com>
2023-01-05 20:15:54 +0100

    update

--- ft_printf/ft_printf_test.c
+++ ft_printf/ft_printf_test.c
@@ -1,13 +1,26 @@
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_test.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/05 19:56:05 by sbouheni          #+#    #+#             */
+/*   Updated: 2023/01/05 20:06:54 by sbouheni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
+#include <stdio.h>
 
 int	main(void)
 {
-	int	length;
-	char *str = "Hello World!";
-	
-	length = ft_printf("%u, %s, %p, %c\n", -150, str, str, 'S');
+	int		length;
+	char	*str;
+
+	str = "Hello World!";
+	length = ft_printf("%x, %s, %p, %c\n", 0, str, str, 'S');
 	ft_printf("%i\n", length);
-	length = printf("%u, %s, %p, %c\n", -150, str, str, 'S');
+	length = printf("%x, %s, %p, %c\n", 0, str, str, 'S');
 	printf("%i\n", length);
 }
