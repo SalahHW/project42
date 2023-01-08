05b614549c1c05c865141df2a4e67f1ace292a47
Salah <salah.bouheniche@gmail.com>
2023-01-08 15:56:22 +0100

    begin get_next_line project

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/08 15:51:55 by sbouheni          #+#    #+#             */
+/*   Updated: 2023/01/08 15:54:37 by sbouheni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+#endif
