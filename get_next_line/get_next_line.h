c06f200088b76fa1890642c467fdb656a50f95fb
SalahHW <salah.bouheniche@gmail.com>
2023-01-09 16:37:26 +0100

    add BUFFER_SIZE

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,14 +6,18 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 15:51:55 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/01/08 19:01:56 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/01/09 16:30:17 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# include <unistd.h>
 # include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE
+# endif
 
 char	*get_next_line(int fd);
 #endif
