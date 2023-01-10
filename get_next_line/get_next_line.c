c87ee90076b9af2f5140e7556b37df528e9660b7
Salah <salah.bouheniche@gmail.com>
2023-01-10 06:14:40 +0100

    try to concatenate strings with join_strings()

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 17:11:31 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/01/09 03:39:22 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/01/10 06:10:28 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,19 @@
 
 char	*get_next_line(int fd)
 {
-	int		i;
-	int		buffer_size;
-	int		read_state;
-	char	*text;
-	char	*line_str;
+	int			i;
+	int			read_state;
+	char		*text;
+	char		*line_str;
 	static int	line_counter;
 
-	buffer_size = 42;
 	i = 0;
 	line_counter = 0;
-	text = malloc(sizeof(char) * buffer_size);
-	read_state = read(fd, text, buffer_size);
-	while (text[i] != '\n' && read_state != 0)
+	text = malloc(sizeof(char) * BUFFER_SIZE);
+	while (read_state != 0)
 	{
-		i++;
+		read_state = read(fd, text, BUFFER_SIZE);
+		join_strings(line_str, text);
 	}
-	line_str = malloc(sizeof(char) * i);
-	i = 0;
-	while (*text != '\n')
-	{
-		line_str[i] = *text;
-		text++;
-		i++;
-	}
-	line_str[i] = *text;
-	line_str[i + 1] = '\0';
 	return (line_str);
 }
