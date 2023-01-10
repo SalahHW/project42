2255ff666983ff084fa7e3f0eb2f526bf4577df8
Salah <salah.bouheniche@gmail.com>
2023-01-10 06:43:32 +0100

    function can read the whole file

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 17:11:31 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/01/10 06:10:28 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/01/10 06:33:17 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,20 @@
 
 char	*get_next_line(int fd)
 {
-	int			i;
-	int			read_state;
-	char		*text;
-	char		*line_str;
-	static int	line_counter;
+	int		read_state;
+	char	*text;
+	char	*line_str;
 
-	i = 0;
-	line_counter = 0;
 	text = malloc(sizeof(char) * BUFFER_SIZE);
+	line_str = malloc(sizeof(char) * BUFFER_SIZE);
+	if (!text || !line_str)
+		return (NULL);
+	read_state = 1;
 	while (read_state != 0)
 	{
 		read_state = read(fd, text, BUFFER_SIZE);
-		join_strings(line_str, text);
+		if (read_state != 0)
+			line_str = join_strings(line_str, text);
 	}
 	return (line_str);
 }
